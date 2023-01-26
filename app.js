94687f296614bc27d5a52ed2e65e311c957a8810
Mohammed Dolapo <106164617+therealhhybrid@users.noreply.github.com>
2023-01-26 07:55:25 +0100

    version edited

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("dec 18, 2022 00:00:00").getTime();
+let countDownDate = new Date("Nov 18, 2023 00:00:00").getTime();
 let x = setInterval(function(){
     let now = new Date().getTime();
     let distance =  countDownDate - now;
